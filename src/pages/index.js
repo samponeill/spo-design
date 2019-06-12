8206c01a98b0b82a0de26de7ef63cb87dd5ee3a7
Sam O'Neill <sam@samponeill.com>
2019-06-12 10:46:36 +0100

    Added new favicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
-import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import Content from '../components/content'
-
 
 const IndexPage = () => (
   <Layout>
