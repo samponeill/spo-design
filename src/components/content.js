447f42d03480bca790121497bbc634c7b3580e23
Sam O'Neill <sam@samponeill.com>
2019-06-12 10:52:45 +0100

    Added component for contact form

--- src/components/content.js
+++ src/components/content.js
@@ -3,6 +3,7 @@ import Branding from '../static/services-01.svg'
 import Marketing from '../static/services-02.svg'
 import Websites from '../static/services-03.svg'
 import Carousel from './carousels'
+import Form from './form'
 
 const Content = () => (
             <main className="section">
@@ -70,38 +71,9 @@ const Content = () => (
                 <h2>Get in touch</h2>
               </div>
 
-            <form
-            id="#message_form" 
-            style={{marginBottom: '10rem'}}
-            name="contact"
-            method="POST"
-            data-netlify="true"
-            netlify-honeypot="bot-field"> 
-                <div className="grid-form">
-                  <div className="chunk form-container">
-                    <label htmlFor="Name">Name*</label>
-                    <input arial-label="name" id="name" required type="text" name="name" style={{width: '100%'}} />
-                  </div>     
-                  <div className="chunk form-container">
-                    <label htmlFor="email">Email*</label>         
-                    <input arial-label="email" id="email" required type="text" name="email" style={{width: '100%'}} />
-                    <input arial-label="Ignore" id="number" type="text" name="number" style={{display: 'none'}} />        
-                  </div>
-                </div>
-                <div className="chunk">
-                  <label htmlFor="Message">Message*</label>
-                  <textarea arial-label="Message box" style={{width: '100%', height: '16rem'}} name="message" cols={40} rows={15} defaultValue={""} />
-                </div>
-              <input className="shadow" arial-label="submit" id="contact-submit" defaultValue="submit" name="submit" type="submit" />
-              <label htmlFor="submit" />
-              <div style={{visibility: "hidden"}} className="hidden chunk">
-                  <label htmlFor="bot-field">I am a robot</label>
-                  <input type="text" name="bot-field" style={{width: '100%'}} />
-              </div>
-            </form>
+            <Form></Form>
             </div>
-            <div className="container"></div>
-            </main>
+          </main>
       );
 
   export default Content
---
+++ src/components/form.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Form = () => (
+            <form
+            id="#message_form" 
+            style={{marginBottom: '10rem'}}
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            netlify-honeypot="bot-field"> 
+                <div className="grid-form">
+                  <div className="chunk form-container">
+                    <label htmlFor="Name">Name*</label>
+                    <input arial-label="name" id="name" required type="text" name="name" style={{width: '100%'}} />
+                  </div>     
+                  <div className="chunk form-container">
+                    <label htmlFor="email">Email*</label>         
+                    <input arial-label="email" id="email" required type="text" name="email" style={{width: '100%'}} />
+                    <input arial-label="Ignore" id="number" type="text" name="number" style={{display: 'none'}} />        
+                  </div>
+                </div>
+                <div className="chunk">
+                  <label htmlFor="Message">Message*</label>
+                  <textarea arial-label="Message box" style={{width: '100%', height: '16rem'}} name="message" cols={40} rows={15} defaultValue={""} />
+                </div>
+              <input className="shadow" arial-label="submit" id="contact-submit" defaultValue="submit" name="submit" type="submit" />
+              <label htmlFor="submit" />
+              <div style={{visibility: "hidden"}} className="hidden chunk">
+                  <label htmlFor="bot-field">I am a robot</label>
+                  <input type="text" name="bot-field" style={{width: '100%'}} />
+              </div>
+            </form>
+    );
+
+  export default Form
