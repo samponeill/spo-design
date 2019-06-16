4e97b61a437f8a3e662702a6c3de213e7637f5e3
Sam O'Neill <sam@samponeill.com>
2019-06-16 06:36:59 +0100

    debugging draft article exclusion

--- gatsby-node.js
+++ gatsby-node.js
@@ -52,7 +52,7 @@ exports.createPages = ({ actions, graphql }) => {
 
   const getArticles = makeRequest(graphql, `
   {
-    allStrapiArticle(where: {draft: {eq: 0}}, sort: {fields: created_at, order: DESC}) {
+    allStrapiArticle(sort: {fields: created_at, order: DESC}) {
       edges {
         node {
           id
