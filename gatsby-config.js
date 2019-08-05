ecfc97600333d5a3e23a59feddc49cc9e316ba91
Sam O'Neill <sam@samponeill.com>
2019-08-05 16:32:03 +0100

    Added gatsby netlify cache plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,8 @@ module.exports = {
     },
     `gatsby-plugin-sass`,
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`    
+    `gatsby-plugin-sharp`,
+    'gatsby-plugin-netlify-cache'
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
