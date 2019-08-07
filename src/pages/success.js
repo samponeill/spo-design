0d0cac032ea30c0721ba61686a87c4a1cfb4dbf5
Sam O'Neill <sam@samponeill.com>
2019-08-07 10:05:14 +0100

    added success page

---
+++ src/pages/success.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layouts"
+
+const NotFoundPage = () => (
+  <Layout>
+    <div className="l-wrapper">
+      <h1>Thanks!</h1>
+      <p>We will get back to you as soon as we can</p>
+    </div>
+  </Layout>
+)
+
+export default NotFoundPage
